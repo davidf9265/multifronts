2aee93b146aefcaf1ee084ce51b7f1b612b8ab26
davidf9265 <david.franco@veevart.com>
2024-09-24 08:16:09 -0500

    storage listener

--- apps/wrapper/src/redux/features/cartSlice.ts
+++ apps/wrapper/src/redux/features/cartSlice.ts
@@ -1,13 +1,23 @@
 import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from '../store';
 
 export interface CartState {
     items: { [key: string]: object | number | string | null }[];
     total: number;
 }
 
+const persistedStoreString = localStorage.getItem(
+    'veevart_apps_store'
+) as string;
+
+const persistedStore = JSON.parse(
+    persistedStoreString || '{}'
+) as unknown as RootState;
 const initialState: CartState = {
-    items: [{ producto: 'Producto 1', precio: 100 }],
+    items: persistedStore?.cartReducer?.items || [
+        { producto: 'Producto 1', precio: 100 },
+    ],
     total: 0,
 };
 
@@ -21,6 +31,11 @@ export const cartSlice = createSlice({
         addToStore: (state) => {
             state.items.push({ id: 1, name: 'Product 1', price: 100 });
         },
+        replaceCart: (state, action: PayloadAction<CartState>) => {
+            console.log(action.payload);
+            state.items = action.payload.items;
+            state.total = action.payload.total;
+        },
         changeTotal: (state, action: PayloadAction<number>) => {
             state.total = action.payload;
         },
@@ -32,6 +47,7 @@ export const cartSlice = createSlice({
 });
 
 // Action creators are generated for each case reducer function
-export const { addToStore, changeTotal, cleanStore } = cartSlice.actions;
+export const { addToStore, changeTotal, cleanStore, replaceCart } =
+    cartSlice.actions;
 
 export default cartSlice.reducer;
--- apps/wrapper/src/redux/store.ts
+++ apps/wrapper/src/redux/store.ts
@@ -1,14 +1,36 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
-import cartReducer from './features/cartSlice';
+import cartReducer, { replaceCart } from './features/cartSlice';
 
 const rootReducer = combineReducers({
     cartReducer,
 });
 
-export const store = configureStore({
+const configuredStore = configureStore({
     reducer: rootReducer,
 });
+// Subscribe to store updates to save to localStorage
+configuredStore.subscribe(() => {
+    const state = configuredStore.getState();
+    localStorage.setItem('veevart_apps_store', JSON.stringify(state));
+});
+
+// mount storage listener
+window.addEventListener('storage', (event) => {
+    if (event?.storageArea && event.storageArea['veevart_apps_store']) {
+        const persistedStoreString = localStorage.getItem(
+            'veevart_apps_store'
+        ) as string;
+
+        const persistedStore = JSON.parse(
+            persistedStoreString || '{}'
+        ) as unknown as RootState;
+
+        configuredStore.dispatch(replaceCart(persistedStore.cartReducer));
+    }
+});
+
+export const store = configuredStore;
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
